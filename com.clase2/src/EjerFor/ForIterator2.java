e2422b029611d957b017afed4b54b37d5f1089fe
JuanManuelCastelo <juanmacastelo1@gmail.com>
2023-05-23 20:27:57 -0300

    Se comitean todos los ejercicios 23/05-

---
+++ com.clase2/src/EjerFor/ForIterator2.java
@@ -0,0 +1,19 @@
+package EjerFor;
+
+public class ForIterator2 {
+    public static void main(String[] args) {
+
+        int j;
+        int suma = 0;
+
+        for (j = 0; j <= 10; j++) {
+            System.out.println("j: [ " + j + "]");
+            suma = suma + j;
+
+        }
+        System.out.println("La suma del acumulador es = " + suma);
+        }
+
+
+    }
+
