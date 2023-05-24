8ef6fbd3a70552f1bc379fadf83c2d69410f0037
JuanManuelCastelo <juanmacastelo1@gmail.com>
2023-05-23 21:38:03 -0300

    Se comitean todos los ejercicios 23/05 V2-

---
+++ com.clase2/src/ArrayList/ArrayList.java
@@ -0,0 +1,17 @@
+package ArrayList;
+
+public class ArrayList {
+    public static void main(String[] args){
+
+   String nombres [] = {"Juan", "Pedro", "Mateo"};
+        for (int i = 0; i < nombres.length; i++) {
+            System.out.println("El nombres: " + nombres[i]);
+
+        }
+
+
+
+    }
+
+
+}
